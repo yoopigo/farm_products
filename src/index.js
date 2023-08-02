ba79cbc237bf0a56636c317f8bbdad62a2116e98
yoopigo <collerggggggg@gmail.com>
2023-08-02 21:05:49 +0500

    create components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/app/app';
+import App from './components/app/App';
 
 const rootElement = createRoot(document.getElementById('root'));
 
