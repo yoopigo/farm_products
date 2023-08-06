67da8b7ac7d735b7ed41a25c7cf0eec07d1cc452
yoopigo <collerggggggg@gmail.com>
2023-08-06 18:06:25 +0500

    create about block

--- src/components/blocks/about/about.jsx
+++ src/components/blocks/about/about.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import './style.css';
+
+import Title, { TitleSize } from '../../ui/title/title';
 
 function About() {
-  return <></>;
+  return (
+    <section className='about'>
+      <div className='about__wrapper'>
+        <Title size={TitleSize.BIG}>
+          Магазин фермерских продуктов с доставкой
+        </Title>
+        <p className='about__text'>
+          Все продукты изготавливаются под заказ. Фермеры начинают готовить
+          продукты за день до отправки заказа клиентам. Именно поэтому мы
+          принимаем заказы заранее и доставляем продукты максимально свежими.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/components/ui/title/title.jsx
+++ src/components/ui/title/title.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './style.css';
 
-function Title() {
-  return <></>;
+const TitleSize = {
+  BIG: 'big',
+  NORMAL: 'normal',
+  SMALL: 'small',
+  DEFAULT: '',
+};
+
+function Title({ children, size }) {
+  return (
+    <h1 className={`title${size ? ` title__${size}` : ''}`}>{children}</h1>
+  );
 }
 
 export default Title;
+export { TitleSize };
