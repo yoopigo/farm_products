673767f3dd93b8c4a9f36f4433e05609102e97df
yoopigo <collerggggggg@gmail.com>
2023-08-06 21:28:37 +0500

    mocks

--- src/components/blocks/advantages-list/advantages-list.jsx
+++ src/components/blocks/advantages-list/advantages-list.jsx
@@ -4,27 +4,24 @@ import Title, { Location } from '../../ui/title/title';
 import Button, { ButtonFlexCenter } from '../../ui/button/button';
 import AdvantagesCard from '../../ui/advantage-card/advantage-card';
 
-function AdvantagesList() {
+function AdvantagesList({ advantages }) {
   return (
     <section className='advantages-list'>
-      <Title location={Location.CENTER}>
-        Почему фермерские продукты лучше?
-      </Title>
-      <ul className='advantages-list__list'>
-        <li className='advantages-list__item'>
-          <AdvantagesCard />
-        </li>
-        <li className='advantages-list__item'>
-          <AdvantagesCard />
-        </li>
-        <li className='advantages-list__item'>
-          <AdvantagesCard />
-        </li>
-        <li className='advantages-list__item'>
-          <AdvantagesCard />
-        </li>
-      </ul>
-      <Button buttonFlex={ButtonFlexCenter.CENTER}>Купить</Button>
+      {advantages?.length ? (
+        <>
+          <Title location={Location.CENTER}>
+            Почему фермерские продукты лучше?
+          </Title>
+
+          <ul className='advantages-list__list'>
+            <li className='advantages-list__item'>
+              <AdvantagesCard />
+            </li>
+          </ul>
+
+          <Button buttonFlex={ButtonFlexCenter.CENTER}>Купить</Button>
+        </>
+      ) : null}
     </section>
   );
 }
---
+++ src/mocks/advantageList.js
@@ -0,0 +1,32 @@
+const advantagesList = [
+  {
+    id: 0,
+    title: 'Еда намного вкуснее',
+    text: 'Домашняя колбаса из мяса, соли и специй и колбаса из магазина — два настолько разных продукта, что они даже не родственники',
+    image: '../assets/badges/eat.svg',
+    badges: 'farm_product',
+  },
+  {
+    id: 1,
+    title: 'Просроченные продукты',
+    text: 'Из-за большого количество товара сотрудники магазинов не успевают своевременно производить замену товара',
+    image: '../assets/badges/garbage.svg',
+    badges: 'shop_product',
+  },
+  {
+    id: 2,
+    title: 'Натуральные продукты',
+    text: 'Поставляем местные органические продукты выращенные без пестицидов и химических удобрений.',
+    image: '../assets/badges/sprout.svg',
+    badges: 'shop_product',
+  },
+  {
+    id: 3,
+    title: 'Некачественное мясо',
+    text: 'Мясные полуфабрикаты, в которых содержится чрезмерно много натрия, вредных жиров, консервантов',
+    image: '../assets/badges/no-meat.svg',
+    badges: 'shop_product',
+  },
+];
+
+export default advantagesList;
