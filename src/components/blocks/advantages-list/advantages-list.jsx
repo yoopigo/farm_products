9fc8703309292c14110aa3e190d850e8887ba5b5
yoopigo <collerggggggg@gmail.com>
2023-08-07 00:10:12 +0500

    mocks

--- src/components/blocks/advantages-list/advantages-list.jsx
+++ src/components/blocks/advantages-list/advantages-list.jsx
@@ -7,21 +7,16 @@ import AdvantagesCard from '../../ui/advantage-card/advantage-card';
 function AdvantagesList({ advantages }) {
   return (
     <section className='advantages-list'>
-      {advantages?.length ? (
-        <>
-          <Title location={Location.CENTER}>
-            Почему фермерские продукты лучше?
-          </Title>
+      <Title location={Location.CENTER}>
+        Почему фермерские продукты лучше?
+      </Title>
+      <ul className='advantages-list__list'>
+        <li className='advantages-list__item'>
+          <AdvantagesCard />
+        </li>
+      </ul>
 
-          <ul className='advantages-list__list'>
-            <li className='advantages-list__item'>
-              <AdvantagesCard />
-            </li>
-          </ul>
-
-          <Button buttonFlex={ButtonFlexCenter.CENTER}>Купить</Button>
-        </>
-      ) : null}
+      <Button buttonFlex={ButtonFlexCenter.CENTER}>Купить</Button>
     </section>
   );
 }
